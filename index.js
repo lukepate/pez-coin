89c598ff10c9247de67c915394eedc345a270092
lukepate <lukewpate@gmail.com>
2021-03-13 17:50:28 -0500

    fix fe

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const Blockchain = require('./blockchain');
 const request = require('request');
 const PubSub = require('./app/pubsub');
@@ -20,6 +21,7 @@ const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
@@ -79,6 +81,10 @@ app.get('/api/wallet-info', (req, res) => {
     })
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/dist/index.html'));
+});
+
 const syncWithRootState = () => {
     request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
         if (!error && response.statusCode === 200) {
@@ -99,6 +105,44 @@ const syncWithRootState = () => {
         }
     })
 };
+
+const walletFoo = new Wallet();
+const walletBar = new Wallet();
+
+const generateWalletTransaction = ({ wallet, recipient, amount }) => {
+    const transaction = wallet.createTransaction({
+        recipient, amount, chain: blockchain.chain
+    });
+
+    transactionPool.setTransaction(transaction);
+};
+
+const walletAction = () => generateWalletTransaction({
+    wallet, recipient: walletFoo.publicKey, amount: 5
+});
+
+const walletFooAction = () => generateWalletTransaction({
+    wallet: walletFoo, recipient: walletBar.publicKey, amount: 10
+});
+
+const walletBarAction = () => generateWalletTransaction({
+    wallet: walletBar, recipient: wallet.publicKey, amount: 15
+});
+
+for (let i=0; i<10; i++) {
+    if (i%3 === 0) {
+        walletAction();
+        walletFooAction();
+    } else if (i%3 === 1) {
+        walletAction();
+        walletBarAction();
+    } else {
+        walletFooAction();
+        walletBarAction();
+    }
+
+    transactionMiner.mineTransactions();
+}
 
 let PEER_PORT;
 
